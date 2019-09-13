fc3673b88913cc0fc51495e0ca962181501ea34f
yatoku <seturidee@mail.ru>
2019-09-13 18:51:53 +0300

    lala

---
+++ app/src/main/java/com/example/myapplication_1/MainActivityProgramErrorTwo.java
@@ -0,0 +1,23 @@
+package com.example.myapplication_1;
+
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainActivityProgramErrorTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.programm_error_2);
+
+    }
+
+}
--- app/src/main/java/com/example/myapplication_1/SplashActivity.java
+++ app/src/main/java/com/example/myapplication_1/SplashActivity.java
@@ -10,15 +10,17 @@ import android.text.style.ForegroundColorSpan;
 import android.view.Window;
 import android.widget.TextView;
 
+import java.util.AbstractCollection;
+
 public class SplashActivity extends Activity {
 
     private TextView textView;
     private Handler handler;
     private long startTime, currentTime, finishedTime = 0L;
-    int black = Color.BLACK;
-    int white = Color.WHITE;
-    private int duration = 7000 / 4;
-    private int endTime = 0;
+    //int black = Color.BLACK;
+    int dColor = Color.rgb(192, 192, 192);
+    private int duration = 8000 / 4;
+    private int endTime = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +29,10 @@ public class SplashActivity extends Activity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.test);
         textView = (TextView) findViewById(R.id.textView1);
-        textView.setText("Loading");
+        textView.setText("Загрузка");
         handler = new Handler();
         startTime = System.currentTimeMillis();
         currentTime = startTime;
-
 
         handler.postDelayed(new Runnable() {
             @Override
@@ -42,15 +43,22 @@ public class SplashActivity extends Activity {
 
                 if (finishedTime >= duration + 30) {
                     startTime = System.currentTimeMillis();
-                    white = black;
+
+                    if( dColor != Color.rgb(192, 192, 192)){
+
+                        dColor = Color.rgb(192, 192, 192);
+                    }
+                    else{
+                        dColor = Color.BLACK;
+                    }
                     handler.postDelayed(this, 0);
+
                 }
                 else {
                     endTime = (int) (finishedTime / 250);
-                    Spannable spannableString = new SpannableString(textView
-                            .getText());
+                    Spannable spannableString = new SpannableString(textView.getText());
                     spannableString.setSpan(new ForegroundColorSpan(
-                                    white), 0, endTime,
+                                    dColor), 0, endTime,
                             Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
 
                     textView.setText(spannableString);
