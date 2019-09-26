81fcd4746c58315c734001a6f0dbad7c753b0711
yatoku <seturidee@mail.ru>
2019-09-26 13:59:52 +0300

    lala

--- app/src/main/java/com/example/myapplication_1/MainActivityCodeScreen.java
+++ app/src/main/java/com/example/myapplication_1/MainActivityCodeScreen.java
@@ -3,6 +3,7 @@ package com.example.myapplication_1;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.EditText;
 
 import androidx.annotation.Nullable;
@@ -33,13 +34,89 @@ public class MainActivityCodeScreen extends AppCompatActivity {
                 if(code_field1.getText().toString().length()==1)
                 {
                     code_field2.requestFocus();
+                }
+            }
 
-                }else if(code_field2.getText().toString().length()==1)
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        code_field2.addTextChangedListener(new TextWatcher() {
+
+            public void onTextChanged(CharSequence s, int start,int before, int count)
+            {
+                if(code_field2.getText().toString().length()==1)
                 {
                     code_field3.requestFocus();
+                }
+            }
 
-                }else{
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            public void afterTextChanged(Editable s) {
+            }
+        });
+        code_field2.addTextChangedListener(new TextWatcher() {
+
+            public void onTextChanged(CharSequence s, int start,int before, int count)
+            {
+                if(code_field2.getText().toString().length()==0)
+                {
+                    code_field2.focusSearch(View.FOCUS_LEFT).requestFocus();
+                }
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        code_field3.addTextChangedListener(new TextWatcher() {
+
+            public void onTextChanged(CharSequence s, int start,int before, int count)
+            {
+                if(code_field3.getText().toString().length()==1)
+                {
                     code_field4.requestFocus();
+                }
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            public void afterTextChanged(Editable s) {
+            }
+        });
+        code_field3.addTextChangedListener(new TextWatcher() {
+
+            public void onTextChanged(CharSequence s, int start,int before, int count)
+            {
+                if(code_field3.getText().toString().length()==0)
+                {
+                    code_field3.focusSearch(View.FOCUS_LEFT).requestFocus();
+                }
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        code_field4.addTextChangedListener(new TextWatcher() {
+
+            public void onTextChanged(CharSequence s, int start,int before, int count)
+            {
+                if(code_field4.getText().toString().length()==0)
+                {
+                    code_field4.focusSearch(View.FOCUS_LEFT).requestFocus();
                 }
             }
 
