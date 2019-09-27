c045f15b44a72de919395f07b2253a21be3c7274
yatoku <seturidee@mail.ru>
2019-09-27 17:28:31 +0300

    lala

--- app/src/main/java/com/example/myapplication_1/MainActivityCodeScreen.java
+++ app/src/main/java/com/example/myapplication_1/MainActivityCodeScreen.java
@@ -3,8 +3,10 @@ package com.example.myapplication_1;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -27,34 +29,30 @@ public class MainActivityCodeScreen extends AppCompatActivity {
         code_field4 = (EditText)findViewById(R.id.ll_code_screen4);
 
 
+
         code_field1.addTextChangedListener(new TextWatcher() {
 
             public void onTextChanged(CharSequence s, int start, int before, int count)
             {
-                if(code_field1.getText().toString().length()==1)
-                {
+                if(code_field1.getText().toString().length()==1) {
                     code_field2.requestFocus();
                 }
             }
 
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                code_field1.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-                    @Override
-                    public void onFocusChange(View view, boolean b) {
-                        code_field1.getText();
-                    }
-                });
             }
 
             public void afterTextChanged(Editable s) {
-                code_field1.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-                    @Override
-                    public void onFocusChange(View view, boolean b) {
-                        code_field1.setText("");
-                    }
-                });
             }
         });
+        
+        code_field1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                code_field1.setText(code_field1.getText());
+            }
+        });
+
 
         code_field2.addTextChangedListener(new TextWatcher() {
 
@@ -73,6 +71,20 @@ public class MainActivityCodeScreen extends AppCompatActivity {
             }
         });
 
+
+        code_field2.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+                if(keyCode == KeyEvent.KEYCODE_DEL) {
+
+                    code_field1.requestFocus();
+                }
+                return false;
+            }
+        });
+
+
         code_field3.addTextChangedListener(new TextWatcher() {
 
             public void onTextChanged(CharSequence s, int start,int before, int count)
@@ -90,67 +102,29 @@ public class MainActivityCodeScreen extends AppCompatActivity {
             }
         });
 
-        code_field4.addTextChangedListener(new TextWatcher() {
 
-            public void onTextChanged(CharSequence s, int start,int before, int count)
-                {
-                        code_field4.requestFocus();
+        code_field3.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+                if(keyCode == KeyEvent.KEYCODE_DEL) {
+
+                    code_field2.requestFocus();
                 }
-
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
-            public void afterTextChanged(Editable s) {
+                return false;
             }
         });
 
-        code_field2.addTextChangedListener(new TextWatcher() {
 
-            public void onTextChanged(CharSequence s, int start,int before, int count)
-            {
-                if(code_field2.getText().toString().length()==0)
-                {
-                    code_field2.focusSearch(View.FOCUS_LEFT).requestFocus();
+        code_field4.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+                if(keyCode == KeyEvent.KEYCODE_DEL) {
+
+                    code_field3.requestFocus();
                 }
-            }
-
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
-
-            public void afterTextChanged(Editable s) {
-            }
-        });
-
-        code_field3.addTextChangedListener(new TextWatcher() {
-
-            public void onTextChanged(CharSequence s, int start,int before, int count)
-            {
-                if(code_field3.getText().toString().length()==0)
-                {
-                    code_field3.focusSearch(View.FOCUS_LEFT).requestFocus();
-                }
-            }
-
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
-
-            public void afterTextChanged(Editable s) {
-            }
-        });
-
-        code_field4.addTextChangedListener(new TextWatcher() {
-
-            public void onTextChanged(CharSequence s, int start,int before, int count)
-            {
-                if(code_field4.getText().toString().length()==0)
-                {
-                    code_field4.focusSearch(View.FOCUS_LEFT).requestFocus();
-                }
-            }
-
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
-
-            public void afterTextChanged(Editable s) {
+                return false;
             }
         });
     }
