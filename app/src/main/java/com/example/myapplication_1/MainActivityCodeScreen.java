4187044125389adaf829536787a718e4d11eb95a
yatoku <seturidee@mail.ru>
2019-10-01 15:47:12 +0300

    lala

--- app/src/main/java/com/example/myapplication_1/MainActivityCodeScreen.java
+++ app/src/main/java/com/example/myapplication_1/MainActivityCodeScreen.java
@@ -6,6 +6,7 @@ import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -27,108 +28,53 @@ public class MainActivityCodeScreen extends AppCompatActivity {
         code_field3 = (EditText)findViewById(R.id.ll_code_screen3);
         code_field4 = (EditText)findViewById(R.id.ll_code_screen4);
 
-
         code_field1.addTextChangedListener(new TextWatcher() {
 
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-                code_field1.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-                    @Override
-                    public void onFocusChange(View view, boolean b) {
-                        if(code_field1.getText().toString().length()==0) {
-                            code_field1.getText();
-                        }
-                    }
-                });
-            }
-
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
-
-            public void afterTextChanged(Editable s) {
-
-                if(code_field1.getText().toString().length()==1) {
+                if( s.length() > 1) {
+                    code_field1.setText( s.subSequence(s.length()-1,s.length()));
+                }else {
                     code_field2.requestFocus();
-                    code_field1.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-                        @Override
-                        public void onFocusChange(View view, boolean b) {
-
-                            code_field1.setText("");
-                        }
-                    });
                 }
-            }
-        });
-
-        code_field2.addTextChangedListener(new TextWatcher() {
-
-            public void onTextChanged(CharSequence s, int start,int before, int count) {
-                code_field2.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-                    @Override
-                    public void onFocusChange(View view, boolean b) {
-                        if(code_field2.getText().toString().length()==0) {
-                            code_field2.getText();
-                        }
-                    }
-                });
             }
 
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
             }
 
-            public void afterTextChanged(Editable s) {
-                if(code_field2.getText().toString().length()==1) {
-                    code_field3.requestFocus();
-                    code_field2.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-                        @Override
-                        public void onFocusChange(View view, boolean b) {
-
-                            code_field2.setText("");
-
-                        }
-                    });
-                }
+            public void afterTextChanged(final Editable s) {
+                
             }
         });
 
-        code_field3.addTextChangedListener(new TextWatcher() {
-
-            public void onTextChanged(CharSequence s, int start,int before, int count) {
-                code_field3.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-                    @Override
-                    public void onFocusChange(View view, boolean b) {
-                        if(code_field3.getText().toString().length()==0) {
-                            code_field3.getText();
-                        }
-                    }
-                });
-            }
-
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
-
-            public void afterTextChanged(Editable s) {
-                if(code_field3.getText().toString().length()==1) {
-                    code_field4.requestFocus();
-                    code_field3.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-                        @Override
-                        public void onFocusChange(View view, boolean b) {
-                            code_field3.setText("");
-                        }
-                    });
-                }
-            }
-        });
-
-        code_field4.setOnKeyListener(new View.OnKeyListener() {
+        code_field2.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
 
                 if(keyCode == KeyEvent.KEYCODE_DEL && event.getAction() == KeyEvent.ACTION_UP) {
 
+                    code_field1.requestFocus();
+                }
+
+                return false;
+            }
+        });
+;
+        code_field2.addTextChangedListener(new TextWatcher() {
+
+            public void onTextChanged(CharSequence s, int start,int before, int count) {
+                if( s.length() > 1) {
+                    code_field2.setText( s.subSequence(s.length()-1,s.length()) );
+                } else {
                     code_field3.requestFocus();
                 }
-                return false;
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            public void afterTextChanged(Editable s) {
+
             }
         });
 
@@ -144,16 +90,53 @@ public class MainActivityCodeScreen extends AppCompatActivity {
             }
         });
 
-        code_field2.setOnKeyListener(new View.OnKeyListener() {
+        code_field3.addTextChangedListener(new TextWatcher() {
+
+            public void onTextChanged(CharSequence s, int start,int before, int count) {
+                if( s.length() > 1) {
+                    code_field3.setText( s.subSequence(s.length()-1,s.length()) );
+                } else {
+                    code_field4.requestFocus();
+                }
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        code_field4.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
 
                 if(keyCode == KeyEvent.KEYCODE_DEL && event.getAction() == KeyEvent.ACTION_UP) {
 
-                    code_field1.requestFocus();
+                    code_field3.requestFocus();
                 }
                 return false;
             }
         });
+
+        code_field4.addTextChangedListener(new TextWatcher() {
+
+            public void onTextChanged(CharSequence s, int start,int before, int count) {
+                if( s.length() > 1) {
+                    code_field4.setText( s.subSequence(s.length()-1,s.length()) );
+                }else{
+                    code_field4.requestFocus();
+                }
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
     }
 }
