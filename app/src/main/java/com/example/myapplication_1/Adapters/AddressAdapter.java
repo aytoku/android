05b4790b5e54999edda3df9f0a2cca9d38e97049
yatoku <seturidee@mail.ru>
2019-10-17 19:56:10 +0300

    lala

--- app/src/main/java/com/example/myapplication_1/Adapters/AddressAdapter.java
+++ app/src/main/java/com/example/myapplication_1/Adapters/AddressAdapter.java
@@ -71,7 +71,6 @@ public class AddressAdapter {
                     notifyDataSetChanged();
                 }
             });
-
         }
 
         @Override
---
+++ app/src/main/java/com/example/myapplication_1/MainActivityComments.java
@@ -0,0 +1,21 @@
+package com.example.myapplication_1;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivityComments extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.comments);
+
+    }
+
+}
