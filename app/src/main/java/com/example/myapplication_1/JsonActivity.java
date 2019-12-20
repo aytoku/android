b2826e161d0255c8b159f359cc55ef1e69aaf1a0
yatoku <seturidee@mail.ru>
2019-12-20 18:40:32 +0300

    lala

--- app/src/main/java/com/example/myapplication_1/JsonActivity.java
+++ app/src/main/java/com/example/myapplication_1/JsonActivity.java
@@ -1,28 +1,25 @@
 package com.example.myapplication_1;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 public class JsonActivity  extends AppCompatActivity {
 
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void main(String[] args){
 
-        setContentView(R.layout.json_parser);
-
- //       Gson gson = new Gson();
         JsonParser jsonParser = new JsonParser();
         String json = "[\\n" +
                 "    {\\n" +
-                "      \"firstname\": \"Gio\",\\n" +
+                "      \"firstName\": \"Gio\",\\n" +
                 "      \"age\": 21,\\n" +
                 "       \"mail\": \"seturidee@mail.ru\"\\n" +
                 "    }]";
         JsonElement jsonElement = jsonParser.parse(json).getAsJsonArray();
-
+        JsonObject jsonObject = jsonElement.getAsJsonObject();
+        String firstName = jsonObject.get("firstName").getAsString();
+        System.out.println(firstName);
     }
 }
